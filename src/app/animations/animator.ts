0fdc0347bdff93511fa69cb0b12917df54e845f5
noahbergh <noahvandenbergh@hotmail.nl>
2022-04-04 14:42:39 +0200

    first commit

---
+++ src/app/animations/animator.ts
@@ -0,0 +1,41 @@
+import { animate, state, style, transition, trigger } from "@angular/animations";
+
+export const transformFormAbove = trigger('transformFromAbove', [
+    transition(':enter', [
+        style({
+            transform: 'translateY(-100%)'
+        }), animate(500, style({
+            transform: 'translateY(0)'
+        }))
+    ])
+])
+export const slide = trigger('slide', [
+    state('left', style({
+        transform: 'translateX(-100%)'
+    })),
+    state('left-absolute', style({
+        transform: 'translateX(-100%)', 
+        position: 'absolute',
+        width: '100%'
+    })),
+    state('invision', style({
+        transform: 'translateX(0)'
+    })),
+    state('invision-absolute', style({
+        transform: 'translateX(0)',
+        position: 'absolute', 
+        width: '100%'
+    })),
+    state('right', style({
+        transform: 'translateX(100%)'
+    })),
+    state('right-absolute', style({
+        transform: 'translateX(100%)',
+        position: 'absolute', 
+        width: '100%'
+    })),
+    transition('left => invision', animate(500)),
+    transition('invision-absolute => right-absolute', animate(500)),
+    transition('invision-absolute => left-absolute', animate(500)),
+    transition('right => invision', animate(500))
+])
---
+++ src/app/redux/combiner.ts
@@ -0,0 +1,17 @@
+export interface IAction<T> {
+  type: string;
+  payload?: T;
+  component: string;
+}
+export interface IActionCreator<T> {
+  payload?: T;
+  component: string
+}
+export interface ISellFetch {
+  email: string;
+  password: string;
+}
+export interface ISellLoginFetch {
+  email: string;
+  password: string;
+}
